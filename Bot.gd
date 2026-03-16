f84736a478384f73c65b731309b99aec26d47aa2
ethan <tyx1993@live.cn>
2026-03-16 23:43:13 +0800

    code optmize

--- Bot.gd
+++ Bot.gd
@@ -4,12 +4,10 @@ class_name Bot
 ## 玩家可见的 Bot API，仅暴露 move_to
 ## 避免玩家通过 get_method_list 等窥探内部实现
 
-var _bot_internal: Node2D
+var _raw_bot
 
-
-func _init(bot_internal: Node2D) -> void:
-	_bot_internal = bot_internal
-
+func _init(raw_bot) -> void:
+	_raw_bot = raw_bot
 
 func move_to(target_x: float, target_y: float) -> void:
-	_bot_internal.move_to(target_x, target_y)
+	_raw_bot.move_to(target_x, target_y)
