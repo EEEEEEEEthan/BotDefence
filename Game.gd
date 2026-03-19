52937ca481b504dfbb9c0c3b3b971278b73c435d
ethan <tyx1993@live.cn>
2026-03-20 00:12:48 +0800

    Remove unused tilemap variable from Game.gd to streamline code

--- Game.gd
+++ Game.gd
@@ -5,7 +5,6 @@ class_name Game
 ## 每个 Bot 通过自己的检视器 Play 按钮独立启动脚本
 ## 通过 stdio 行协议与 Python 子进程交互，无需 TCP 服务器
 
-@onready var tilemap: TileMapLayer = $%TileMapLayer
 @onready var scripts_panel: Control = $%ScriptsPanel
 
 func _ready() -> void:
